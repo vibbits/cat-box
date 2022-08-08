29054c38f907685d5cf1721a6ebfa26bfd7a52b3
James Collier <james.collier@vib.be>
2022-08-08 10:13:42 +0200

    Use the rextester API to evaluate Python

--- rextester.js
+++ rextester.js
@@ -1,17 +1,30 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 const body = {
-    LanguageChoice: 24,
-    Program: "1+1",
-    Input: "",
-    CompilerArgs: "",
+  LanguageChoice: 24,
+  Program: `print(${process.argv[process.argv.length - 1]})`,
+  Input: "",
+  CompilerArgs: "",
+  ApiKey: process.env.REXTESTER_API_KEY,
 };
 
-const response = await fetch('https://rextester.com/rundotnet/api', {
-	method: 'post',
-	body: JSON.stringify(body),
-	headers: {'Content-Type': 'application/json'}
+const response = await fetch("https://rextester.com/rundotnet/api", {
+  method: "post",
+  body: JSON.stringify(body),
+  headers: { "Content-Type": "application/json" },
 });
 const data = await response.json();
 
-console.log(data);
+console.log(`print(${process.argv[process.argv.length - 1]})`);
+
+if (data.Errors) {
+  console.log(`Error:\n${data.Errors}`);
+} else {
+  console.log(`Result:\n${data.Result}`);
+}
+
+if (data.Warnings) {
+  console.log(data.Warnings);
+}
+
+console.log(data.Stats);
